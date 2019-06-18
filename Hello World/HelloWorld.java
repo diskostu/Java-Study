1bf72cf0eb061762b69070da86f31275354f7021
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2019-06-18 00:38:01 -0300

    Hello world !

--- Hello World/HelloWorld.java
+++ Hello World/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
+        // Gotta start somewhere , i guess
         System.out.println("Hello World !");
     }
 }
