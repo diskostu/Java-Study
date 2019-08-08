130c270db2a232487002cd96f23b03b6deded064
Gileade Trindade Silva <gileade.trindade@gmail.com>
2019-08-08 18:33:29 -0300

    Alterações Gile

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,57 +1,66 @@
+import java.util.Scanner;
+
 /* 
     A quick (No knowledge) calculator made using Java , which i dont know nothing Yet
     I have some knowledge in Python , C# , C and GO , and now i want to learn Java
 
     EDIT : I know there are some errors , but in the essence , i quite got it right , right ?
 */
-import com.sun.java_cup.internal.runtime.Scanner;
+//import com.sun.java_cup.internal.runtime.Scanner;
 
-public class calculator{
-    public static void main(String[] args) {
-        float result;
-        char operator;
-        char newCalc = 'y';
+public class Calculator {
+	public static void main(String[] args) {
+		float result = 0;
+		char operator = '+';
+		String newCalc = "y";
 
-        System.out.println("Enter a value for A :");
-        int valueA  =   Scanner.nextInt();
 
-        System.out.println("Enter a value for B :");
-        int valueB =   Scanner.nextInt();
+		while (newCalc == "y") {
 
-        while (newCalc == 'Y') {
-            
-            System.out.println("Choose one of the following operations :");
-            System.out.println("1 for Addition");
-            System.out.println("2 for Subtraction :");
-            System.out.println("3 for Multiplication");
-            System.out.println("4 for Division");
+			System.out.println("Enter a value for A :");
+			Scanner sc = new Scanner(System.in);
 
-            int optionCalc =   Scanner.next();
+			int valueA = sc.nextInt();
 
-            
-            switch(optionCalc){
-                case    1   :
-                    operator    =   '+';
-                    result  =   valueA  +   valueB;
-                case    2   :   
-                    operator    =   '-';
-                    result  =   valueA  -   valueB;
-                case    3   :   
-                    operator    =   'x';
-                    result  =   valueA  *   valueB;
-                case    4   :
-                    operator    =   '/';   
-                    result  =   valueA  /   valueB;
-            }
-        
-            System.out.println("The result of "+valueA+" "+operator+" "+valueB+" = "+result);
-            
-            System.out.println("Would you like to do another calculation ? [Y/N]");
-            newCalc =   Scanner.next();
+			System.out.println("Enter a value for B :");
+			int valueB = sc.nextInt();
 
-            if (newCalc != 'y' && newCalc != 'y'){
-                break;
-            }
-        }
-    }
+
+			System.out.println("Choose one of the following operations :");
+			System.out.println("1 for Addition");
+			System.out.println("2 for Subtraction :");
+			System.out.println("3 for Multiplication");
+			System.out.println("4 for Division");
+
+			int optionCalc = sc.nextInt();
+
+			switch (optionCalc) {
+			case 1:
+				operator = '+';
+				result = valueA + valueB;
+				break;
+			case 2:
+				operator = '-';
+				result = valueA - valueB;
+				break;
+			case 3:
+				operator = 'x';
+				result = valueA * valueB;
+				break;
+			case 4:
+				operator = '/';
+				result = valueA / valueB;
+				break;
+			}
+
+			System.out.println("The result of " + valueA + " " + operator + " " + valueB + " = " + result);
+
+			System.out.println("Would you like to do another calculation ? [Y/N]");
+			newCalc = sc.next();
+
+			if (newCalc != "y" || newCalc != "Y") {
+				newCalc = "N";
+			}
+		}
+	}
 }
