d29bc4453033b61e17eab821baa0ca70fda70136
Lucas Felipe Dalamarta <lfdalamarta@hotmail.com>
2019-08-12 23:00:48 -0300

    Updated Calc

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,36 +1,25 @@
 import java.util.Scanner;
 
-/* 
-    A quick (No knowledge) calculator made using Java , which i dont know nothing Yet
-    I have some knowledge in Python , C# , C and GO , and now i want to learn Java
-
-    EDIT : I know there are some errors , but in the essence , i quite got it right , right ?
-*/
-//import com.sun.java_cup.internal.runtime.Scanner;
-
 public class Calculator {
 	public static void main(String[] args) {
-		float result = 0;
-		char operator = '+';
-		String newCalc = "y";
+		float   result 		= 	0;
+        char    operator 	= 	'+';
+        Boolean newCalc 	=	true;
 
-
-		while (newCalc == "y") {
-
-			System.out.println("Enter a value for A :");
+		while (newCalc) {
 			Scanner sc = new Scanner(System.in);
 
+			System.out.println("Enter a value for A :");
 			int valueA = sc.nextInt();
 
 			System.out.println("Enter a value for B :");
 			int valueB = sc.nextInt();
 
-
 			System.out.println("Choose one of the following operations :");
-			System.out.println("1 for Addition");
-			System.out.println("2 for Subtraction :");
-			System.out.println("3 for Multiplication");
-			System.out.println("4 for Division");
+			System.out.println("\t1 for Addition");
+			System.out.println("\t2 for Subtraction :");
+			System.out.println("\t3 for Multiplication");
+			System.out.println("\t4 for Division");
 
 			int optionCalc = sc.nextInt();
 
@@ -53,14 +42,21 @@ public class Calculator {
 				break;
 			}
 
-			System.out.println("The result of " + valueA + " " + operator + " " + valueB + " = " + result);
+			System.out.println("\n\n\tThe result of " + valueA + " " + operator + " " + valueB + " = " + result);
 
-			System.out.println("Would you like to do another calculation ? [Y/N]");
-			newCalc = sc.next();
+			System.out.println("\nWould you like to do another calculation ? [Y/N]");
+			String cont =	sc.next();
 
-			if (newCalc != "y" || newCalc != "Y") {
-				newCalc = "N";
+			if (cont.equals("n")){
+				newCalc	=	false;
+			}
+			else if(cont.equals("y")){
+				newCalc	=	true;
+			}
+			else{
+				newCalc = false;
 			}
 		}
+		System.out.println("\nThanks for using Luk3's Calculator");
 	}
 }
